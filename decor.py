547638e9cad116bc6448ad4706340c6c1b88bf13
Rachel Fishkin <rachelfishkin@MacBook-Pro-Rachel.local>
2022-03-27 03:04:18 +0300

    first commit

---
+++ decor.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import os
+
+path = '/Users/rachelfishkin/Desktop/Studying/pro'
+def parametrized_decor(parameter):
+    def decor(foo):
+        def new_foo(*args, **kwars):
+            with open(os.path.join(parameter, 'gener.txt'), "w") as file:
+                t = str(datetime.now())
+                result = foo(*args, **kwars)
+                name = foo.__name__
+                string = f'{t}, {name}, {args}, {result}, {parameter}'
+                file.write(string)
+                result = foo(*args, **kwars)
+                return result 
+        return new_foo
+    return decor
+ 
+@parametrized_decor(parameter=path)
+def flat_generator(nested_list): 
+    for a in sum(nested_list,[]):
+        yield a
+
+nested_list = [
+	['a', 'b', 'c'],
+	['d', 'e', 'f'],
+	['g', 'h', 'i'],
+]
+for item in  flat_generator(nested_list):
+	print(item)
